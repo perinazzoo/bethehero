02856d8809bfab2588aae0cc97af7a606cf8ea7d
perinazzoo <gabrielperinazo@gmail.com>
2020-03-28 17:40:40 -0300

    :construction: add yup validation

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,8 +1,21 @@
 import { Op } from 'sequelize';
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+      whatsapp: Yup.string().required(),
+      address: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Incorrect provided data' });
+    }
+
     const { name, email, password, whatsapp, address } = req.body;
 
     const userExists = await User.findOne({
