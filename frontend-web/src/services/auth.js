3ade15dff76bd9712e6d412b6cc61340f1d88bab
perinazzoo <gabrielperinazo@gmail.com>
2020-04-04 04:06:29 -0300

    :construction: change auth and api services

--- frontend-web/src/services/auth.js
+++ frontend-web/src/services/auth.js
@@ -2,16 +2,15 @@ export const STORAGE_KEY = '@bethehero';
 export const isAuthenticated = () => {
   const current = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
+  if (!current) {
+    return false;
+  }
   if (current.logout === true) {
     return false;
   }
-  if (new Date() < current.expireAt) {
+  if (new Date() < new Date(current.expireAt * 1000)) {
     return true;
   }
-  localStorage.setItem(
-    STORAGE_KEY,
-    JSON.stringify({ ...current, expired: true })
-  );
   return false;
 };
 export const login = (token, name, expireAt) => {
@@ -21,12 +20,16 @@ export const login = (token, name, expireAt) => {
       token,
       name,
       logout: false,
-      expired: false,
       expireAt,
     })
   );
 };
 
 export const logout = () => {
-  localStorage.removeItem(STORAGE_KEY);
+  const current = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify({ ...current, logout: true })
+  );
 };
