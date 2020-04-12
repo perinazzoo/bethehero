aa43f6ddaf66c144a7b3fb1a40d2d4df6c87e26a
perinazzoo <gabrielperinazo@gmail.com>
2020-04-12 03:27:19 -0300

    :lock: remove response after creating an user

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -27,7 +27,7 @@ class UserController {
       return res.status(400).json({ error: 'User already exists' });
     }
 
-    const user = await User.create({
+    await User.create({
       name,
       email,
       password,
@@ -35,7 +35,7 @@ class UserController {
       address,
     });
 
-    return res.json(user);
+    return res.json();
   }
 
   async update(req, res) {
